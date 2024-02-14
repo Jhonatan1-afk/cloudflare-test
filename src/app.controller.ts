3ceb1e73c769a31bb9537d06402550cf6c6d8ed4
Jhonatan Caicedo <jonatan9911@gmail.com>
2024-02-14 11:07:42 -0500

    feat(): add root path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hi')
+  @Get()
+  root(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('hi')
   getHello(): string {
     return this.appService.getHello();
   }
